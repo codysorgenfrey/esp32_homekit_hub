8f546c21eec5184c0e6965fbb37ec88b10d61ddc
Cody Sorgenfrey <cosorgen@microsoft.com>
2022-01-28 14:23:44 -0800

    initial setuping

---
+++ common.h
@@ -0,0 +1,37 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include "secrets.h"
+
+// Board stuff
+#define STATUS_LED BUILTIN_LED
+#define PWR_LED 16
+#define LED_ON LOW
+#define LED_OFF HIGH
+
+// Heat pump stuff
+#define HP_MANUFACTURER "Mitsubishi-Electric"
+#define HP_NAME "Split-System-Heat-Pump"
+#define HP_MODEL "MSZ-GL12NA"
+// #define HP_SERIALNUM "88N11458" // Andy's room
+// #define HP_SERIALNUM "88N11476" // Dining room
+#define HP_SERIALNUM "88N11479" // Livng room
+// #define HP_SERIALNUM "88N11426" // Master bed room
+#define HP_UNIQUE_NAME HP_NAME "-" HP_SERIALNUM
+#define HP_DEFAULT_HEAT_THRESH 18.8889 // 66 Deg F
+#define HP_DEFAULT_COOL_THRESH 23.3333 // 74 Deg F
+
+// Homekit stuff
+#define HK_DEBUG false
+#define SKETCH_VER "0.0.1"
+
+// State handling
+#define STATUS_ERROR 0
+#define STATUS_OK 1
+#define STATUS_NO_WIFI 2
+#define STATUS_NO_OTA 3
+#define STATUS_NO_HOMEKIT 4
+#define STATUS_NO_HEAT_PUMP 5
+#define STATUS_OTA_PROGRESS 6
+
+#endif
--- homekit_hub.ino
+++ homekit_hub.ino
@@ -1,9 +1,122 @@
+#include "common.h"
+#include <arduino_homekit_server.h> // need to disable logging in homekit_debug.h
+#include <ArduinoOTA.h>
+#include <WiFiManager.h>
+
+extern "C" homekit_server_config_t config;
+
+WiFiManager wm;
+
+unsigned long int boardStatus = STATUS_NO_WIFI;
+
+int statusBlinkPattern[7] = {
+    10, // Error
+    0,  // ok
+    1,  // no wifi
+    2,  // No OTA
+    3,  // No Homkit
+    4,  // no heat pump
+    3   // OTA progress
+};
+int statusPatternRate[7] = {
+    2000, // error
+    1000, // ok
+    1000, // no wifi
+    1000, // No OTA
+    1000, // No homekit
+    1000, // no heat pump
+    2000  // OTA progress
+};
+unsigned long lastBlinkMs = 0;
+
+void handleStatus() {
+    unsigned long nowMs = millis();
+    int diff = nowMs - lastBlinkMs;
+    if (abs(diff) >= statusPatternRate[boardStatus]) {
+        if (boardStatus == STATUS_OK && digitalRead(STATUS_LED) == LED_ON) {
+            digitalWrite(STATUS_LED, LED_OFF);
+        } else {
+            for (int x = 0; x < statusBlinkPattern[boardStatus]; x++) {
+                digitalWrite(STATUS_LED, LED_ON);
+                delay(99);
+                digitalWrite(STATUS_LED, LED_OFF);
+                delay(99);
+            }
+        }
+
+        lastBlinkMs = nowMs;
+    }
+}
+
 void setup()
 {
-	
+    // Setup output LED
+    pinMode(STATUS_LED, OUTPUT); // set up status LED
+    pinMode(PWR_LED, OUTPUT);    // set up power LED
+    digitalWrite(STATUS_LED, LED_OFF);
+    digitalWrite(PWR_LED, LED_ON);
+
+    #if HK_DEBUG
+        Serial.begin(115200);
+    #endif
+
+    if (boardStatus == STATUS_NO_WIFI) {
+        // Connect to wifi
+        wm.setDebugOutput(HK_DEBUG);
+        wm.setConfigPortalBlocking(false);
+        wm.setSaveConfigCallback([](){ 
+            boardStatus = STATUS_NO_OTA;
+            setup();
+        });
+
+        boardStatus = wm.autoConnect(HP_UNIQUE_NAME) ? STATUS_NO_OTA : STATUS_NO_WIFI;
+    }
+    
+    if (boardStatus == STATUS_NO_OTA) {
+        // Connect OTA
+        ArduinoOTA.setHostname(HP_UNIQUE_NAME);
+        ArduinoOTA.setPassword(OTA_PASS);
+        ArduinoOTA.setRebootOnSuccess(true);
+        ArduinoOTA.onStart([](){ 
+            boardStatus = STATUS_OTA_PROGRESS;
+            #if HK_DEBUG
+                Serial.println("Starting OTA Update");
+            #endif
+        });
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) { handleStatus(); });
+        ArduinoOTA.onError([](ota_error_t error) { 
+            boardStatus = STATUS_ERROR;
+            #if HK_DEBUG
+                Serial.printf("Error[%u]: ", error);
+                if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                else if (error == OTA_END_ERROR) Serial.println("End Failed");
+            #endif
+        });
+        ArduinoOTA.begin();
+        
+        // Connect to Homekit
+        boardStatus = STATUS_NO_HOMEKIT;
+        arduino_homekit_setup(&config);
+    }
 }
 
 void loop()
 {
-	
+    // Handle OTA 
+    if (boardStatus != STATUS_NO_OTA) ArduinoOTA.handle();
+    
+    // Status blinks
+    handleStatus();
+
+    // Handle WiFi
+    if (boardStatus == STATUS_NO_WIFI) {
+        wm.process();
+        return;
+    }
+
+    // Handle Homekit
+    if (boardStatus != STATUS_NO_HOMEKIT) arduino_homekit_loop();
 }
